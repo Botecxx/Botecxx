dc57c9e14fa954aa3995aa05ce01384553135b84
Botecxx <hfbs7135@gmail.com>
2024-07-22 21:44:04 -0400

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.TrashSpawnRate = 60000 -- Time in milliseconds
+Config.TrashBagModel = 'prop_rub_binbag_01'
+Config.GarbageTruckModel = 'trash'
+Config.JobNPC = {
+    model = 's_m_y_garbage',
+    coords = vector3(-1103.8, -1692.5, 4.4),
+    heading = 0.0
+}
+Config.JobStartCoords = vector3(-1103.8, -1692.5, 4.4)
