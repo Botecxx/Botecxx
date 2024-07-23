5edbef0f086bd120eafe950fd47329e11dfb5052
Botecxx <hfbs7135@gmail.com>
2024-07-22 21:43:34 -0400

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+    ['garbage_job'] = 'Garbage Job',
+    ['start_job'] = 'Start Job',
+    ['invite_players'] = 'Invite Players',
+    ['close_menu'] = 'Close Menu',
+    ['pickup_trash'] = 'Pick Up Trash',
+    ['place_trash'] = 'Place Trash in Truck',
+    ['payment_received'] = 'You received $%s for your work!',
+}
